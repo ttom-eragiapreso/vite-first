1fcbca00bd7f650e75d37c2b45f0fca096036f41
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-11-14 14:04:13 +0100

    Finito

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+  <script>
+import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    
+  components: { AppTitle },
+    name: "App",
+  }
+  </script>
+
+
+<template>
+  <AppTitle/>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+
+<script>
+export default {
+  name: "AppTitle",
+  data(){
+    return {
+      title: "La mia prima app con Vite!"
+    }
+  }
+  }
+
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<style>
+  h1 {
+    color: brown;
+  }
+</style>
